525a4cba37d2449f2c376952ce4b6549de9bf5a7
Richard Ma <rma15@siebl-0224-17.ews.illinois.edu>
2021-04-24 22:17:36 -0500

    add user interface

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <map>
 #include <iterator>
+#include <iomanip>
 #include <list>
 
 using std::cout;
@@ -40,9 +41,13 @@ map<string, Actor*> actors;
 map<string, string> actor_name_to_id;
 bool isFirstLine;
 
+Actor* getActorFromName(string name) {
+    return actors[actor_name_to_id[name]];
+}
+
 string find_movie_in_common(string actor_name_1, string actor_name_2) {
-    Actor* actor1 = actors[actor_name_to_id[actor_name_1]];
-    Actor* actor2 = actors[actor_name_to_id[actor_name_2]];
+    Actor* actor1 = getActorFromName(actor_name_1);
+    Actor* actor2 = getActorFromName(actor_name_2);
 
     set<string> common_movies;
     for (auto x : actor1->getMovies()) {
@@ -57,19 +62,20 @@ string find_movie_in_common(string actor_name_1, string actor_name_2) {
 }
 
 void parseMovieNames(int numOfMoviesToRead) {
-    ifstream read_titles_txt("./data/titles.txt");
+    ifstream read_titles_txt("../data/titles.txt");
     // parse movies
     isFirstLine = true;
 
     string line;
 
+    cout << "Parsing movies...\n";
     while (getline(read_titles_txt, line)) {
         // ignore first line
         if (isFirstLine) {
             isFirstLine = false;
             continue;
         }
-        if (numOfMoviesToRead < 0) {
+        if (numOfMoviesToRead == 0) {
             break;
         }
         // split the text
@@ -84,14 +90,17 @@ void parseMovieNames(int numOfMoviesToRead) {
         string_to_movie[words[0]] = m; // put entry in map
         numOfMoviesToRead--;
     }
-    cout << "done parsing " << string_to_movie.size() << " movies" << '\n';
+    cout << "Done parsing " << string_to_movie.size() << " movies" << '\n';
 }
 
 void parseMovieRatings() {
     // parse ratings for movies that are already in the map
-    ifstream read_ratings_txt("./data/ratings.txt");
+    ifstream read_ratings_txt("../data/ratings.txt");
     isFirstLine = true;
     string line;
+
+    cout << "Parsing movie ratings...\n";
+    int numRatingsParsed = 0;
     while (getline(read_ratings_txt, line)) {
         if (isFirstLine) {
             isFirstLine = false;
@@ -109,16 +118,17 @@ void parseMovieRatings() {
         }
         Movie* m = string_to_movie[words[0]];
         m->addRatingInfo(words[1], words[2]);
+        numRatingsParsed++;
     }
-    cout << "done parsing ratings" << '\n';
+    cout << "Done parsing " << numRatingsParsed << " ratings" << '\n';
 }
 
 void parseActors(int numOfActorsToRead) {
     // parse actors
-    ifstream read_actors_txt("./data/actors.txt");
+    ifstream read_actors_txt("../data/actors.txt");
     isFirstLine = true;
     
-
+    cout << "Parsing actors\n";
     string line;
     while (getline(read_actors_txt, line)) {
         if (isFirstLine) {
@@ -159,7 +169,7 @@ void parseActors(int numOfActorsToRead) {
         }
         numOfActorsToRead--;
     }
-    cout << "done parsing " << actors.size() << " actors" << '\n';
+    cout << "Done parsing " << actors.size() << " actors" << '\n';
 }
 
 void parseEdges() {
@@ -181,7 +191,7 @@ void parseEdges() {
             }
         }
     }
-    cout << "done parsing edges" << '\n';
+    cout << "Done parsing edges" << '\n';
 }
 
 void BFS_to_bacon(string requested_actor) {
@@ -227,8 +237,8 @@ void BFS_to_bacon(string requested_actor) {
         vector<string> chain;
         while (true) {
             chain.push_back(actor);
-            if (visited[actors[actor_name_to_id[actor]]] != "root") {
-                actor = visited[actors[actor_name_to_id[actor]]];
+            if (visited[getActorFromName(actor)] != "root") {
+                actor = visited[getActorFromName(actor)];
             } else {
                 break;
             }
@@ -244,21 +254,106 @@ void BFS_to_bacon(string requested_actor) {
 
 }
 
+map<pair<Actor*, Actor*>, int> FloydWarshall() {
+    map<pair<Actor*, Actor*>, int> distance; // use this for floyd warshall
+    int INF = 1000000; // arbitrary large number
+
+    cout << "Starting floyd warshall...\n";
+    // initialization- if same node then dist = 0, else set to INF
+    for (auto i = actors.begin(); i != actors.end(); i++) {
+        for (auto j = actors.begin(); j != actors.end(); j++) {
+            if (i == j) {
+                distance[{i->second, j->second}] = 0;
+            }
+            else {
+                distance[{i->second, j->second}] = INF;
+            }
+        }
+    }
+
+    // now for the nodes with edges, set distance = 1
+    for (auto i = actors.begin(); i != actors.end(); i++) {
+        std::set<Actor*> adjacentActors = (i->second)->getAdjacent();
+        for (auto x : adjacentActors) {
+            distance[{i->second, x}] = 1;
+        }
+    }
+    cout << "hi\n";
+    // do the DP
+    for (auto k = actors.begin(); k != actors.end(); k++) {
+        for (auto i = actors.begin(); i != actors.end(); i++) {
+            for (auto j = actors.begin(); j != actors.end(); j++) {
+                distance[{i->second, j->second}] = min(distance[{i->second, j->second}], distance[{i->second, k->second}]+distance[{k->second, j->second}]);
+            }
+        }
+    }
+    cout << "Done with floyd warshall\n";
+    return distance;
+}
+
+void printHelp() {
+    cout << "Hello, welcome to the IMDB databank\n";
+    cout << "Press 1 to start a Kevin Bacon Search using BFS\n";
+    cout << "Press 2 for a broader search between two individuals using Floyd Warshall\n";
+    cout << "Press 3 for analytics on the 'Six Degrees of Kevin Bacon' theory\n";
+    cout << "Press 4 to terminate the program\n";
+    cout << "Press 0 to see this help menu again\n";
+}
+
+string promptUserInput() {
+    printHelp();
+    string userInput;
+    getline(cin, userInput);
+    return userInput;
+}
+
 int main() {
 
+    // pre user-input processing
     int numOfMoviesToRead = 3000000; // file is 7 million lines long 
     parseMovieNames(numOfMoviesToRead);
 
     parseMovieRatings();
 
-    int numOfActorsToRead = 100000; // file is 10 million lines long 
+    int numOfActorsToRead = 200; // file is 10 million lines long 
     parseActors(numOfActorsToRead);
 
     parseEdges();
-    
-    string requested_actor;
-    cout << "Please enter in an actor and we will find the path to Kevin Bacon.\n";
-    getline(cin, requested_actor);
-
-    BFS_to_bacon(requested_actor);
+    // map<pair<Actor*, Actor*>, int> distance = FloydWarshall();
+    while (true) {
+        string userInput = promptUserInput();
+        if (userInput == "0") {
+            printHelp();
+        } else if (userInput == "1") {
+            string requested_actor;
+            cout << "Please enter in an actor and we will find the path to Kevin Bacon.\n";
+            getline(cin, requested_actor);
+            BFS_to_bacon(requested_actor);
+        } else if (userInput == "2") {
+            string actor1, actor2;
+            cout << "Please enter in your first actor\n";
+            getline(cin, actor1);
+            cout << "Please enter in your second actor\n";
+            getline(cin, actor2);
+            Actor* a1 = getActorFromName(actor1);
+            Actor* a2 = getActorFromName(actor2);
+            cout << "The distance is " << distance[{a1, a2}] << " actors long" << '\n';
+        } else if (userInput == "3") {
+            int numActors = actors.size();
+            int numActorsWithDistLessThanSix = 0;
+            Actor* kevin = getActorFromName("Kevin Bacon");
+            for (auto i = actors.begin(); i != actors.end(); i++) {
+                if (distance[{kevin, i->second}] <= 6) {
+                    numActorsWithDistLessThanSix++;
+                }
+            }
+            double percent = numActorsWithDistLessThanSix / numActors;
+            cout << numActorsWithDistLessThanSix << " out of " << numActors << " had a distance less than or equal to 6 to Kevin Bacon\n";
+            cout << setprecision(2) << "That's " << percent << "%!\n";
+        } else if (userInput == "4") {
+            cout << "Thanks for using the IMDB databank. Have a nice day!\n";
+            break;
+        }
+    }
+    return 0;
 }
