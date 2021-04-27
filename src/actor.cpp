e5707515c0ab715fed4e02a1ca5fb6d05c9d9417
Ma, Richard <rma15@illinois.edu>
2021-04-26 19:22:21 -0500

    Update actor.cpp

--- src/actor.cpp
+++ src/actor.cpp
@@ -6,10 +6,20 @@ Actor::Actor(string actor_name, string birth_year, string death_year, vector<Mov
     this->birth_year = birth_year;
     this->death_year = death_year;
     this->movies = movies;
+    is_director = false;
+    calculateLastMovie();
 }
 
 void Actor::addAdjacent(Actor* a) {
     acted_with.insert(a);
+}
+
+// taken from https://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
+bool Actor::is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
 }
 
 std::set<Actor*> Actor::getAdjacent() {
@@ -39,4 +49,44 @@ map<Actor*, int> Actor::getEdges() {
 
 int Actor::getEdge(Actor* a) {
     return edges[a];
+}
+
+void Actor::setDirector(bool val) {
+    is_director = val;
+}
+bool Actor::getIsDirector() {
+    return is_director;
+}
+
+Movie* Actor::getLastMovie() {
+    return last_movie;
+}
+void Actor::calculateLastMovie() {
+    int newest_movie_age = 0;
+    Movie* ret;
+    for (auto x : movies) {
+        if (!is_number(x->getYear())) {
+            continue;
+        }
+        int year = std::stoi(x->getYear());
+        if (year > newest_movie_age) {
+            ret = x;
+            newest_movie_age = year;
+        }
+    }
+    last_movie = ret;
+}
+
+std::set<Actor*> Actor::getInfluenced() {
+    return influenced;
+}
+std::set<Actor*> Actor::getInfluencedBy() {
+    return influenced_by;
+}
+void Actor::addInfluence(Actor* person) {
+    influenced.insert(person);
+}
+
+void Actor::addInfluenceBy(Actor* person) {
+    influenced_by.insert(person);
 }
