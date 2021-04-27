d42e0656d3a8693fcc664a37ff0f04250cabf28a
Ma, Richard <rma15@illinois.edu>
2021-04-26 19:22:08 -0500

    Update actor.h

--- src/actor.h
+++ src/actor.h
@@ -20,14 +20,32 @@ public:
     map<Actor*, int> getEdges();
     void addEdge(Actor* a, int d);
     int getEdge(Actor* a);
+    void setDirector(bool val);
+    bool getIsDirector();
+    Movie* getLastMovie();
+    void calculateLastMovie();
+    std::set<Actor*> getInfluenced();
+    std::set<Actor*> getInfluencedBy();
+    void addInfluence(Actor* person);
+    void addInfluenceBy(Actor* person);
+    bool is_number(const std::string& s);
 
 private:
     string actor_name;
     string birth_year;
     string death_year;
+    bool is_director;
     vector<string> genres;
     vector<Movie*> movies;
-    // these house the actor pointers, not the names or ids
+    Movie* last_movie;
+
+    // these house the actor pointers, not the names or ids. For BFS, undirected unweighted
     std::set<Actor*> acted_with;
+    // for Dijkstra, undirected, weighted
     map<Actor*, int> edges;
+    // for Kosaraju, directed, unweighted
+    // the outbound edges. A -> B if A influenced B
+    std::set<Actor*> influenced;
+    // the inbound edges. Need this for the graph reversal
+    std::set<Actor*> influenced_by;
 };
